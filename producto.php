0c5397e5876432de5ec98ccdbc24e4386f2b0528
RocioLopz <roci.alo24@gmail.com>
2024-10-08 17:36:34 -0300

    add producto

---
+++ producto.php
@@ -0,0 +1,60 @@
+<?php
+
+    require_once "utils/funciones.php";
+    require_once "utils/db_connection.php";
+
+        /* Capturamos la tabla que viene por GET (URL) */
+
+        
+        $tabla = $_GET['categoria'] ?? false;
+
+       /* Tablas alidas */
+       $tablas = [
+            'naves' => 'naves',
+            'peliculas' => 'peliculas',
+            'personajes' => 'personajes',
+            'sables' => 'sables'
+       ];
+
+       /* Comprobar si el array existe */
+
+       if (!array_key_exists($tabla, $tablas)) {
+        header('Location: error404.php');
+       }
+
+        /* Llamar a la funcionm */
+
+        $categorias = listar_todo($conn, $tabla);
+
+        /*  echo "<pre>";
+        var_dump($categorias);
+        echo "</pre>";  */
+
+?>
+
+<?php require "partials/header.php" ?>
+
+<main class="container">
+<img src="imagenes/peliculas_portada.jpg" class="rounded-circle mx-auto d-block" alt="...">
+    <div class="row">
+        <h1 class="text-center"></h1>
+
+        <?php foreach($categorias as $producto) { ?>
+            <div class="col-4 mt-4 mb-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/<?=$producto['imagen'];?>" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title"><?=$producto['titulo'];?></h5>
+                            <h5 class="cart-title text-succes"><?=$producto['episodio'];?></h5>
+                            <a href="producto_particular.php?categorias=<?= $tabla ?>&id=<?= $producto['id']?>" class="btn btn-primary">Ver</a>
+                        </div>
+
+                    </div>
+                </div>
+
+        <?php } ?>
+    </div>
+
+</main>
+
+<?php require "partials/footer.php" ?>
